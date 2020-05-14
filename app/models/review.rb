fbdf6941c7de64ee7f877b53b65ed5079a63421e
Philippe Niemann <pniemann@orange.fr>
2020-05-14 09:21:41 +0200

    rails app restaurant 130520

---
+++ app/models/review.rb
@@ -0,0 +1,6 @@
+class Review < ApplicationRecord
+  validates :content, presence: true
+  validates :rating, inclusion: { in: 0..5 }, numericality: { only_integer: true }, presence: true
+  belongs_to :restaurant
+
+end
--- db/seeds.rb
+++ db/seeds.rb
@@ -5,3 +5,16 @@
 #
 #   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
 #   Character.create(name: 'Luke', movie: movies.first)
+restaurant1 = Restaurant.create!(name:"Perchoir", address:"Paris", phone_number: "0123456789", category: "french" )
+bristol = Restaurant.new(name: "Epicure", category: "french", address: "75008 Paris", phone_number: "0110233445")
+nagata = Restaurant.new(name: "Nagata", category: "japanese", address: "75011 Paris", phone_number: "0120233445")
+roma = Restaurant.new(name: "Roma", category: "italian", address: "75011 Paris", phone_number: "0130233445")
+kaoya = Restaurant.new(name: "Kaoya", category: "chinese", address: "75011 Paris", phone_number: "0140233445")
+friteunefois = Restaurant.new(name: "Frite une fois", category: "belgian", address: "75011 Paris", phone_number: "0150233445")
+
+
+
+# restaurant1.save
+
+review1 = Review.create!(content:"Toto", rating:"3", restaurant: restaurant1)
+# review1.save
